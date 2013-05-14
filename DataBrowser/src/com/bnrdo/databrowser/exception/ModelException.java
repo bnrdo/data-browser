51ebea8504f889fa331574d89bb6af74315c7b4c
bnrdo <nardssantos@yahoo.com>
2013-05-14 17:28:10 +0800

    Wrapper for exceptions in the model layer

---
+++ DataBrowser/src/com/bnrdo/databrowser/exception/ModelException.java
@@ -0,0 +1,11 @@
+package com.bnrdo.databrowser.exception;
+
+public class ModelException extends RuntimeException {
+    public ModelException() {
+        super();
+    }
+
+    public ModelException(String message) {
+        super(message);
+    }
+}
